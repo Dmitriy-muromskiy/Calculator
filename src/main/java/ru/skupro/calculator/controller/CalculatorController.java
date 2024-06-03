c919be1661ebd5882e6013659844db5887480271
79206 <serdukovd.a.murom@yandex.ru>
2024-06-03 20:46:11 +0300

    ready task commit

--- src/main/java/ru/skupro/calculator/controller/CalculatorController.java
+++ src/main/java/ru/skupro/calculator/controller/CalculatorController.java
@@ -1,4 +1,48 @@
 package ru.skupro.calculator.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skupro.calculator.service.CalculatorService;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String greeting() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + " + " + num2 + " = " + calculatorService.plus(num1, num2);
+
+    }
+    @GetMapping("/minus")
+    public String minus(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
+
+    }
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
+
+    }
+    @GetMapping("/divide")
+    public String divide(@RequestParam Integer num1, @RequestParam Integer num2) {
+        if (num2 ==0){
+            return " Деление на 0 запрещено";
+        }
+        return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
+
+    }
 }
